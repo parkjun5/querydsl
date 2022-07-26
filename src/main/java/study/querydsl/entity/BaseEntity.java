361ace93857b28ca538c1480acd72b19150daf16
박세준 <sjpark@selab.co.kr>
2022-07-26 09:49:14 +0900

    Initial commit

---
+++ src/main/java/study/querydsl/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package study.querydsl.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime updateDate;
+
+}
